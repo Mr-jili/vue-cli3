0785cdcc7e6fb0b2e0f194e463c9d50ab934b503
jili <jili52012@126.com>
2019-01-24 17:18:38 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? './' : '/', //部署应用包时的基本 URL
+  publicPath: process.env.NODE_ENV === 'production' ? './' : './', //部署应用包时的基本 URL
   outputDir: 'dist', //当运行 vue-cli-service build 时生成的生产环境构建文件的目录
   assetsDir: 'assets', //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
   lintOnSave: false, // eslint-loader 是否在保存的时候检查   安装@vue/cli-plugin-eslint有效
@@ -40,6 +40,9 @@ module.exports = {
         changeOrigin: true
       },
       '/test': { //测试
+        target: 'xxx'
+      },
+      '/pre': {  //预发布
         target: 'xxx'
       },
       '/pro': { //正式
